cb5097f13dea39f091e4d0983dd750ce777934fb
Nori Onesty <noriww08091993@hotmail.com>
2026-09-18 21:07:42 +0200

    retry decorator for network operations

--- utils.py
+++ utils.py
@@ -1,40 +1,22 @@
-import functools
 import time
-from typing import Callable, Any, Dict
+import functools
+from typing import Callable, Any, Type, Tuple
 
-CACHE: Dict[tuple, Any] = {}
-
-def memoize(func: Callable) -> Callable:
-    @functools.wraps(func)
-    def wrapper(*args: Any, **kwargs: Any) -> Any:
-        key = (func.__name__, args, frozenset(kwargs.items()))
-        if key not in CACHE:
-            CACHE[key] = func(*args, **kwargs)
-        return CACHE[key]
-    return wrapper
-
-class BatchProcessor:
-    def __init__(self, size: int = 100):
-        self.size = size
-        self.buffer = []
-
-    def process(self, item: Any, callback: Callable) -> None:
-        self.buffer.append(item)
-        if len(self.buffer) >= self.size:
-            callback(self.buffer)
-            self.buffer.clear()
-
-def timer(func: Callable) -> Callable:
-    @functools.wraps(func)
-    def wrapper(*args: Any, **kwargs: Any) -> Any:
-        start = time.perf_counter()
-        result = func(*args, **kwargs)
-        end = time.perf_counter()
-        return result, end - start
-    return wrapper
-
-def fast_flatten(nested: list) -> list:
-    return [item for sublist in nested for item in sublist]
-
-def clear_cache() -> None:
-    CACHE.clear()
+def retry(exceptions: Tuple[Type[Exception], ...] = (Exception,), 
+          tries: int = 3, 
+          delay: float = 1.0, 
+          backoff: float = 2.0) -> Callable:
+    def decorator(func: Callable) -> Callable:
+        @functools.wraps(func)
+        def wrapper(*args: Any, **kwargs: Any) -> Any:
+            mtries, mdelay = tries, delay
+            while mtries > 1:
+                try:
+                    return func(*args, **kwargs)
+                except exceptions:
+                    time.sleep(mdelay)
+                    mtries -= 1
+                    mdelay *= backoff
+            return func(*args, **kwargs)
+        return wrapper
+    return decorator
